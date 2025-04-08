ca15ebcead33b88d02b5148ecbfa335be06c03af
enzo-thomaz <enzo.jesus@aluno.senai.br>
2025-04-08 08:50:48 -0300

    Arquivo main

---
+++ Digite_dois_valores_e _uma_operação/Digite_dois_valores_e_uma_operacão.py
@@ -0,0 +1,19 @@
+# -*- cod: UTF-8 -*-
+
+print("Digite dois valores e uma operação ( +, -, *, /)e eu te darei o resultado")
+
+def contadora ():
+    num1 = int(input("digite o primeiro valor: "))
+    operador = input("digite uma operação: ")
+
+    if operador == '+':
+        print ("você escolhei somar os valores, o resultado é: ", soma)
+
+    elif operador == '-':
+        subt = num1 - num2
+
+    elif operador == '*':
+        mult = num1 * num2
+
+    elif operador == '/':
+        print ("você escolheu dividir, o resultado é: ", divi)
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# s2_enzo_jesus
+# s2_enzo_jesus O meu projeto se resume em dar dos valores e uma operação, sendo em divisão,soma, multiplicação e subtração. Eu te darei o resultado de cada uma.
  
